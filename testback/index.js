d7e5ed5b0db1e6325496447b4d47663378a37608
PrakashChandra <46310172+prakashnerale@users.noreply.github.com>
2020-04-10 15:18:13 +0530

    Update index.js

--- testback/index.js
+++ testback/index.js
@@ -1,3 +1,31 @@
 const express = require("express");
 
 const app = express();
+
+const port = 8000;
+app.get("/", (req,res) => {
+   return res.send("Home Page")})
+
+app.get("/signup", (req,res) => {
+   return res.send("SignUp Page")})
+
+app.get("/Signin", (req,res) => {
+    return res.send("SignIn Page")})
+
+app.get("/SignOut", (req,res) => {
+    return res.send("SignOut Page")})
+
+
+
+app.listen(port, () => 
+   console.log("Server is up and Running..."))
+
+
+
+
+
+//const port = 3000
+
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+//app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`)
